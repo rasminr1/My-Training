882382eb94615c3ddb6ea24e15e7349f72c5cf37
rasminr1 <124284259+rasminr1@users.noreply.github.com>
2023-02-04 22:55:44 +0530

    upload my files to git repository

---
+++ MyProject/src/one/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package one.test;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		HelloWorld.display();
+
+	}
+	public static void display() {
+		System.out.println("hai");
+	}
+
+}
